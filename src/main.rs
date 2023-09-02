f4a8cce9a2a9d0c22c5e758e4286c7a82e754b0f
Nathan McIntosh <natemcintosh@gmail.com>
2023-09-02 13:48:12 -0400

    realized can cut branches. Runs ~5x faster

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,26 @@ use fastrand;
 /// actually keep track of the ones at the top and bottom. Then pick random directions
 /// for those two ants, and calculate the max time. `ant_vel_mps` is the ant velocity in
 /// meters/second
-fn fast_ant_sim(n_ants: usize, ant_vel_mps: f64) -> f64 {
+fn ant_sim(n_ants: usize, ant_vel_mps: f64) -> f64 {
     std::iter::repeat_with(|| fastrand::f64())
         // Simulate n_ants
         .take(n_ants)
         // Calculate the distance for each ant
-        .map(|pos| {
-            // If true, then facing down, and travel from their current position to 0.0
-            if fastrand::bool() {
-                pos / ant_vel_mps
-            } else {
-                // Otherwise facing up, and have to travel from current position to 1.0
-                (1.0 - pos) / ant_vel_mps
-            }
-        })
+        .map(|pos| pos / ant_vel_mps)
         // Return the max
         .max_by(|a, b| a.partial_cmp(b).expect("Found a NaN in the ant positions"))
         .expect("No ants were simulated")
 }
 
 fn main() {
-    let n_sims = 1_000;
+    let n_sims = 10_000;
     let n_ants = 10_000;
 
     let start_time = std::time::Instant::now();
 
     let times: Vec<f64> = (0..n_sims)
         .into_iter()
-        .map(|_| fast_ant_sim(n_ants, 0.01))
+        .map(|_| ant_sim(n_ants, 0.01))
         .collect();
 
     let run_time = start_time.elapsed();
